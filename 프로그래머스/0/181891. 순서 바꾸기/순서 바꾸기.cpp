31898f46eb5d2d93354d765c705295cce7518daa
backsani <118903562+backsani@users.noreply.github.com>
2024-11-15 02:06:25 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer;
+    
+    for(int i = 0; i < num_list.size() - n; i++){
+        answer.push_back(num_list[i + n]);
+    }
+    
+    for(int i = 0; i < n; i++){
+        answer.push_back(num_list[i]);
+    }
+    
+    return answer;
+}
