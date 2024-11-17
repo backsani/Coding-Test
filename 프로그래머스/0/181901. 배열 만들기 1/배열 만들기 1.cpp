60cae7a52c25ac87f9c818b939ac6701f70a30c7
backsani <118903562+backsani@users.noreply.github.com>
2024-11-17 21:34:23 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.70 ms, Memory: 4.32 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int k) {
+    vector<int> answer;
+    
+    for(int i = 1; i*k <= n; i++){
+        answer.push_back(i*k);
+    }
+    
+    return answer;
+}
