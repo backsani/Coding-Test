d06b00de469122211c8a802e3b692a11f6b80791
backsani <118903562+backsani@users.noreply.github.com>
2024-11-17 22:34:46 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.cpp
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.cpp
@@ -1,36 +1,27 @@
 #include <string>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
 int solution(string binomial) {
     int answer = 0;
-    string a, b, op;
-    int trans = 1;
+    int a, b;
+    char op;
     
-    for(char c : binomial){
-        if(c == ' '){
-            trans++;
-        }
-        else if(trans == 1){
-            a += c;
-        }
-        else if(trans == 2){
-            op += c;
-        }
-        else if(trans == 3){
-            b += c;
-        }
-    }
+    stringstream ss;
+    ss.str(binomial);
+    ss >> a >> op >> b;
     
-    if(op == "+"){
-        answer = stoi(a) + stoi(b);
+    
+    if(op == '+'){
+        answer = (a) + (b);
     }
-    else if(op == "-"){
-        answer = stoi(a) - stoi(b);
+    else if(op == '-'){
+        answer = (a) - (b);
     }
-    else if(op == "*"){
-        answer = stoi(a) * stoi(b);
+    else if(op == '*'){
+        answer = (a) * (b);
     }
     
     return answer;
