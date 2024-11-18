f7dd5fbb0781f87c7c5b7b3dec8505bcbaae1617
backsani <118903562+backsani@users.noreply.github.com>
2024-11-18 23:09:27 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.22 ms, Memory: 6.01 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> arr, int idx) {
+    int answer = -1;
+    
+    for(int i{}; i < arr.size(); i++){
+        if(idx <= i){
+            if(arr[i] == 1)
+                return i;
+        }
+    }
+    
+    return answer;
+}
