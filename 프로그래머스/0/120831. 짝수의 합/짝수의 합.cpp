12d5678979f065460f08f29e8ed47e4de2026ec7
backsani <118903562+backsani@users.noreply.github.com>
2024-12-09 23:51:00 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for(int i = 0; i < n + 1; i += 2)
+    {
+        answer += i;
+    }
+    return answer;
+}
