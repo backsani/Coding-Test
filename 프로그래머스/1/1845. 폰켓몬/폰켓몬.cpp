535e74eb0facff5077194175b5218ce1be17902d
backsani <118903562+backsani@users.noreply.github.com>
2024-12-14 22:09:23 +0900

    [level 1] Title: 폰켓몬, Time: 0.59 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int numsSize = nums.size() / 2;
+    sort(nums.begin(), nums.end());
+    nums.erase(unique(nums.begin(), nums.end()), nums.end());
+    return nums.size() > numsSize ? numsSize : nums.size();
+}
