e37e1a9b0464e0f032105fed005cdd523ea9398f
backsani <118903562+backsani@users.noreply.github.com>
2025-01-10 23:48:26 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string rsp) {
+    string answer = "";
+    for(char ch : rsp)
+    {
+        if(ch == '2') answer += "0";
+        if(ch == '0') answer += "5";
+        if(ch == '5') answer += "2";
+    }
+    return answer;
+}
