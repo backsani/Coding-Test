7f739e650bf8e7d00a9e6c8f675d8f9acd90f757
backsani <118903562+backsani@users.noreply.github.com>
2025-02-03 01:47:45 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string skill, vector<string> skill_trees) {
+    int answer = 0;
+    
+    for(string str : skill_trees)
+    {
+        bool can = true;
+        int i = 0;
+        for(char ch : str)
+        {
+            int it = skill.find(ch);
+            if(it != string::npos)
+            {
+                if(i == it)
+                {
+                    i++;
+                }
+                else
+                {
+                    can = false;
+                    break;
+                }
+                    
+            }
+        }
+        
+        if(can)
+            answer++;
+    }
+    
+    
+    return answer;
+}
