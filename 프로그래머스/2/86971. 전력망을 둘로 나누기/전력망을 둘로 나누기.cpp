65dc2e727de1d1ccc433bfde2605d271a2b8112b
backsani <118903562+backsani@users.noreply.github.com>
2025-02-10 21:18:35 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.03 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,57 +1,32 @@
-#include <string>
-#include <vector>
-#include <set>
-#include <climits>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int solution(int n, vector<vector<int>> wires) {
-    int answer = -1;
-    int min = INT_MAX;
-    vector<vector<int>> temp = wires;
-    
-    for(int i = 0; i < wires.size(); i++)
-    {
-        set<int> a;
-        set<int> b;
-        int aValue = wires[i][0];
-        int bValue = wires[i][1];
-        
-        a.insert(wires[i][0]);
-        b.insert(wires[i][1]);
-        
-        while(a.size() + b.size() != n)
-        {
-            for(int j = 0; j < wires.size(); j++)
-            {
-                if(i != j)
-                {
-
-                    auto a0 = a.find(wires[j][0]);
-                    auto a1 = a.find(wires[j][1]);
-                    if(a0 != a.end() || a1 != a.end())
-                    {
-                        a.insert(wires[j][0]);
-                        a.insert(wires[j][1]);
-                    }
-
-                    auto b0 = b.find(wires[j][0]);
-                    auto b1 = b.find(wires[j][1]);
-                    if(b0 != b.end() || b1 != b.end())
-                    {
-                        b.insert(wires[j][0]);
-                        b.insert(wires[j][1]);
-                    }
-                }
-            }
-        }
-        
-        int result = a.size() - b.size();
-        result = result > 0 ? result : result * -1;
-        
-        min = result < min ? result : min;
+    vector<vector<int>> graph(n + 1);
+    for(int i = 0; i < (int)wires.size(); i++) {
+        int u = wires[i][0];
+        int v = wires[i][1];
+        graph[u].push_back(v);
+        graph[v].push_back(u);
     }
-    
-    return min;
+    vector<int> siz(n + 1);
+    function<void(int, int)> dfs = [&](int cur, int prev)  -> void {
+        siz[cur] = 1;
+        for(int nxt : graph[cur]) {
+            if(nxt == prev) continue;
+            dfs(nxt, cur);
+            siz[cur] += siz[nxt];
+        }
+    };
+    dfs(1, -1);
+    int answer = INT_MAX;
+    for(int i = 1; i <= n; i++) {
+        for(int j : graph[i]) {
+            int l = siz[j];
+            int r = n - siz[j];
+            answer = min(answer, abs(l - r));
+        }
+    }
+    return answer;
 }
