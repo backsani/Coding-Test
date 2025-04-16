f554e857c6225a95b2adc28c88231535088b6fb9
backsani <118903562+backsani@users.noreply.github.com>
2025-04-17 08:02:09 +0900

    [level 1] Title: 소수 만들기, Time: 5.14 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -1,57 +1,20 @@
 #include <vector>
 #include <iostream>
-#include <algorithm>
-#include <set>
-
 using namespace std;
-
-int solution(vector<int> n) {
-    vector<int> nums = n;
+bool check(int n){
+    for(int i=2;i<n/2;i++)
+        if(n%i==0)
+            return false;
+    return true;
+}
+int solution(vector<int> nums) {
     int answer = 0;
-    int debug = 0;
-    vector<int> number;
-
-    vector<bool> select(nums.size(), false);
-    fill(select.begin(), select.begin() + 3, true);
-    
-    do
-    {
-        int temp = 0;
-        for(int i = 0; i < nums.size(); i++)
-        {
-            if(select[i])
-                temp += nums[i];
-        }
-        number.push_back(temp);
-    }while(prev_permutation(select.begin(), select.end()));
-
-    sort(number.begin(), number.end());
-    
-    vector<bool> prime(number.back(), true);
-    cout << number.back() << endl;
-    
-    prime[0] = false;
-    for(int i = 2; i * i <= prime.size(); i++)
-    {
-        int j = 2;
-        while(i * j <= prime.size())
-        {
-            prime[i*j - 1] = false;
-            j++;
-        }
-    }
-    
-    for(int num : number)
-    {
-        if(prime[num - 1] == true)
-        {
-            
-            answer++;
-        }
-    }
-    
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    
-
+    int N=nums.size();
+    for(int i=0;i<N;i++)
+        for(int j=i+1;j<N;j++)
+            for(int k=j+1;k<N;k++){
+                if(check(nums[i]+nums[j]+nums[k]))
+                    answer++;
+            }
     return answer;
 }
