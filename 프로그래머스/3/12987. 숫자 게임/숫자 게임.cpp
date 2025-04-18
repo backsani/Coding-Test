92fe74390a8abab7a8892dd0ec9b1ace08a604bd
backsani <118903562+backsani@users.noreply.github.com>
2025-04-18 13:06:58 +0900

    [level 3] Title: 숫자 게임, Time: 11.23 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B) {
+    int answer = -1;
+    int count = 0;
+    
+    int frontPoint = 0;
+    int backPoint = A.size() - 1;
+    
+    sort(A.rbegin(), A.rend());
+    sort(B.begin(), B.end());
+    
+    for(int i = 0; i < A.size(); i++)
+    {
+        if(B[i] > A[backPoint])
+        {
+            backPoint--;
+            count++;
+        }
+        else if(B[i] > A[frontPoint])
+        {
+            frontPoint++;
+            count++;
+        }
+        else
+            frontPoint++;
+    }
+    
+    return count;
+}
