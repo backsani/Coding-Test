858af1bc91cf15414a3582a30fbb61ba2b1241cc
backsani <118903562+backsani@users.noreply.github.com>
2025-08-29 17:14:07 +0900

    [level 3] Title: 섬 연결하기, Time: 0.08 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,76 +1,39 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
+int d[101];
+
+int getParent(int node){
+    if(node == d[node]){
+        return node;
+    }
+    else return d[node] = getParent(d[node]);
+}
+
+bool compare(vector<int> a, vector<int> b){
+    return a[2] < b[2];
+}
 
 int solution(int n, vector<vector<int>> costs) {
+
     int answer = 0;
-    
-    sort(costs.begin(), costs.end(), [](vector<int>& a, vector<int>& b){
-        return a[2] < b[2];
-    });
-    
-    vector<vector<int>> groups;
-    
-    for(vector<int> v : costs)
-    {
-        bool conect = false;
-        bool newIsland = true;
-        int index = -1;
-        
-        for(int i = 0; i < groups.size(); i++)
-        {
-            auto g0 = find(groups[i].begin(), groups[i].end(), v[0]);
-            auto g1 = find(groups[i].begin(), groups[i].end(), v[1]);
-            
-            if (g0 != groups[i].end() && g1 != groups[i].end())
-            {
-                newIsland = false;
-                continue;
-            }
-            if(g0 != groups[i].end())
-            {
-                if(conect)
-                {
-                    groups[i].insert(groups[i].end(), groups[index].begin(), groups[index].end());
-                    groups.erase(groups.begin() + index);
-                    break;
-                }
-                
-                newIsland = false;
-                groups[i].push_back(v[1]);
-                answer += v[2];
-                conect = true;
-                index = i;
-            }
-            else if(g1 != groups[i].end())
-            {
-                if(conect)
-                {
-                    groups[i].insert(groups[i].end(), groups[index].begin(), groups[index].end());
-                    groups.erase(groups.begin() + index);
-                    break;
-                }
-                
-                newIsland = false;
-                groups[i].push_back(v[0]);
-                answer += v[2];
-                conect = true;
-                index = i;
-            }
-        }
-        
-        if(newIsland)
-        {
-            vector<int> temp;
-            temp.push_back(v[0]);
-            temp.push_back(v[1]);
-            answer += v[2];
-            
-            groups.push_back(temp);
+    for(int i =0; i<n; i++){
+        d[i] = i;
+    }
+    sort(costs.begin(), costs.end(), compare);
+    for(int i=0; i<costs.size(); i++){
+        int start = getParent(costs[i][0]);
+        int end = getParent(costs[i][1]);
+        int cost = costs[i][2];
+
+        if(start != end){
+            d[end] = start;
+
+            answer += cost;
         }
     }
-    
+
+
     return answer;
 }
