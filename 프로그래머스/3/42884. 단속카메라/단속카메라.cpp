aef6e5bc7e742c26762944df38f0936999467065
backsani <118903562+backsani@users.noreply.github.com>
2025-10-14 23:36:01 +0900

    [level 3] Title: 단속카메라, Time: 0.51 ms, Memory: 3.89 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -1,46 +1,23 @@
 #include <string>
 #include <vector>
-#include <climits>
 #include <algorithm>
 
 using namespace std;
 
 int solution(vector<vector<int>> routes) {
-    int answer = 0;
-    
-    vector<vector<int>> beginRoutes = routes;
-    
-    sort(beginRoutes.begin(), beginRoutes.end(), [](vector<int>& a, vector<int>& b){
-        return a[0] < b[0];
-    });
-    sort(routes.begin(), routes.end(), [](vector<int>& a, vector<int>& b){
+    sort(routes.begin(), routes.end(), [](const vector<int>& a, const vector<int>& b){
         return a[1] < b[1];
     });
-    
-    int routesIndex = 0;
-    int beginIndex = 0;
-    
-    int lastPos = INT_MIN;
-    
-    while(routesIndex < routes.size() && routesIndex != routes.size())
-    {
-        if(lastPos >= routes[routesIndex][0])
-        {
-            routesIndex++;
-            continue;
+
+    int camera = -30001; 
+    int answer = 0;
+
+    for (const auto& route : routes) {
+        if (route[0] > camera) {
+            camera = route[1];
+            answer++;
         }
-        
-        int temp = routes[routesIndex][1];
-        
-        while(beginIndex < routes.size() && beginRoutes[beginIndex][0] <= temp)
-        {
-            beginIndex++;
-        }
-        
-        lastPos = temp;
-        answer++;
-        routesIndex++;
     }
-    
+
     return answer;
 }
