aa9a0bf9433991838bc62e864ac61d6cc7a7a81c
backsani <118903562+backsani@users.noreply.github.com>
2026-01-01 18:30:44 +0900

    [level 2] Title: 완전범죄, Time: 0.02 ms, Memory: 3.73 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.cpp
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.cpp
@@ -7,7 +7,6 @@ using namespace std;
 int solution(vector<vector<int>> info, int n, int m) {
     const int INF = 1e9;
 
-    // dp[b] = B 흔적이 b일 때 A 흔적 최소
     vector<int> dp(m, INF);
     dp[0] = 0;
 
@@ -20,13 +19,11 @@ int solution(vector<vector<int>> info, int n, int m) {
         for (int b = 0; b < m; b++) {
             if (dp[b] == INF) continue;
 
-            // 1) A가 훔치는 경우: A 증가, B 동일
             int na = dp[b] + aCost;
             if (na < n) {
                 ndp[b] = min(ndp[b], na);
             }
 
-            // 2) B가 훔치는 경우: B 증가, A 동일
             int nb = b + bCost;
             if (nb < m) {
                 ndp[nb] = min(ndp[nb], dp[b]);
