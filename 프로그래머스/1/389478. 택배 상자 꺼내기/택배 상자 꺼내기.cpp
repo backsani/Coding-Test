e0278e056aee7bb40d7f1f62b37f1b9b4e19656a
backsani <118903562+backsani@users.noreply.github.com>
2026-01-03 02:00:30 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <stack>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n, int w, int num) {
+    int answer = 1;
+    
+    vector<stack<int>> v(w, stack<int>());
+    bool right = true;
+    int index = 0;
+    
+    for(int i = 1; i <= n; i++)
+    {
+        if(index >= w)
+        {
+            right = false;
+            index--;
+        }
+        else if(index < 0)
+        {
+            right = true;
+            index++;
+        }
+        
+        v[index].push(i);
+        
+        index += right ? 1 : -1;
+    }
+    
+    int floor = ceil((float)num / w);
+    index = floor % 2 != 0 ? num % w - 1 : (w - (num % w)) % w;
+    if(index == -1)
+        index = w - 1;
+    
+    while(v[index].size() != 0)
+    {
+        if(v[index].top() == num)
+            return answer;
+        
+        v[index].pop();
+        answer++;
+    }
+    
+    return answer;
+}
