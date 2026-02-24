9164b7e486c5e2338eb8f2a72d9a5491be5c8fa8
backsani <118903562+backsani@users.noreply.github.com>
2026-02-24 12:32:11 +0900

    [level 3] Title: 여행경로, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -6,67 +6,37 @@
 
 using namespace std;
 
-vector<string> BFS(vector<vector<string>>& t)
-{
+vector<string> solution(vector<vector<string>> tickets) {
     vector<string> answer;
     
-    sort(t.begin(), t.end(), [](const vector<string>& a, const vector<string>& b){
-        return a[1] < b[1];
-    });
+    map<string, priority_queue<string, vector<string>, greater<string>>> m;
     
-    map<string, int> m;
-    
-    int index = 1;
-    for(vector<string> ticket : t)
+    for(vector<string> ticket : tickets)
     {
-        if(m[ticket[0]] == 0)
-            m[ticket[0]] = index++;
-        if(m[ticket[1]] == 0)
-            m[ticket[1]] = index++;
+        m[ticket[0]].push(ticket[1]);
     }
     
-    vector<vector<string>> des(m.size() + 1, vector<string>());
-    for(vector<string> v : t)
-    {
-        des[m[v[0]]].push_back(v[1]);
-    }
+    vector<string> st;
+    st.push_back("ICN");
     
-    // {{ 남은 경로, 진행한 경로}, 다음 목적지}
-    queue<pair<pair<vector<vector<string>>, vector<string>>, string>> q;
-    vector<vector<string>> temp = des;
-    q.push({ { des, vector<string>() }, "ICN"});
-    
-    while(!q.empty())
+    while(!st.empty())
     {
-        pair<pair<vector<vector<string>>, vector<string>>, string> p = q.front();
-        q.pop();
+        string current = st.back();
         
-        vector<vector<string>> les = p.first.first;
-        vector<string> answer = p.first.second;
-        string prev = p.second;
-        
-        answer.push_back(prev);
-        
-        vector<vector<string>> lesQ = les;
-        for(int i = 0; i < lesQ[m[prev]].size(); i++)
+        if(m[current].empty())
         {
-            string next = lesQ[m[prev]][i];
-            vector<vector<string>> nextQ = lesQ;
-            nextQ[m[prev]].erase(nextQ[m[prev]].begin() + i);
-            
-            if(answer.size() == t.size())
-            {
-                answer.push_back(next);
-                return answer;
-            }
-            
-            q.push({{nextQ, answer}, next});
+            answer.push_back(current);
+            st.pop_back();
+        }
+        else
+        {
+            string next = m[current].top();
+            m[current].pop();
+            st.push_back(next);
         }
     }
     
-    return {};
-}
-
-vector<string> solution(vector<vector<string>> tickets) {
-    return BFS(tickets);
+    reverse(answer.begin(), answer.end());
+    
+    return answer;
 }
